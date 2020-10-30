cb97ad8b5d15566d46fa9be74baaa6c5bd883fe9
Matthew Milici <milici.m915@gmail.com>
2020-10-29 20:23:39 -0700

    deploying

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 app.listen(PORT, function() {
     console.log(`Now listening on port: ${PORT}`);
